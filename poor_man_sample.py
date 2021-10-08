31ae4e78d884bea444147ab8f5a67ac31d0b1183
magdalenahamel <30760661+magdalenahamel@users.noreply.github.com>
2021-10-08 14:58:49 -0300

    Update poor_man_sample.py

--- poor_man_sample.py
+++ poor_man_sample.py
@@ -107,6 +107,7 @@ for l in range(len(bs)):
     for i in range(len(csize)):
         for j in range(len(hs)):
             for k in range(len(hv)):
+                print(l,i,j,k)
                 exp_fill_fac = sample.Sample(prob_hit_log_lin,200,sample_size=300, csize=csize[i], h=hs[j], hv=hv[k])
                 e3_a_1 = exp_fill_fac.Nielsen_sample(2,bs[l],0.2)
                 results_Wr.append(e3_a_1[8])
