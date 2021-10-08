048890104534b5e9baa113f270e8abb90363bb4c
magdalenahamel <30760661+magdalenahamel@users.noreply.github.com>
2021-10-08 17:30:40 -0300

    Update poor_man_sample.py

--- poor_man_sample.py
+++ poor_man_sample.py
@@ -115,9 +115,9 @@ for l in range(len(bs)):
                 results_R_vir.append(e3_a_1[7])
                 
                 
-results_Wr_r = np.reshape(results_Wr, (5,5,5,5,300))
-results_D_r = np.reshape(results_D, (5,5,5,5,300))
-results_R_vir_r = np.reshape(results_R_vir, (5,5,5,5,300))
+results_Wr_r = np.reshape(results_Wr, (7,7,7,7,300))
+results_D_r = np.reshape(results_D, (7,7,7,7,300))
+results_R_vir_r = np.reshape(results_R_vir, (7,7,7,7,300))
 results_r = [results_Wr_r, results_D_r, results_R_vir_r]
 
 np.save('mcmc_2', results_r)
