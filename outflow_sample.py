8fbd883839680a74d1dc18c6768338f835c79693
magdalenahamel <30760661+magdalenahamel@users.noreply.github.com>
2021-12-05 23:10:19 +0100

    Update outflow_sample.py

--- outflow_sample.py
+++ outflow_sample.py
@@ -563,6 +563,7 @@ for i in range(2):
         for k in range(2):
             for l in range(2):
                 for m in range(2):
+                    print(i,j,k,l,m)
                     bla = get_sample(Ns[i],theta_maxs[j],0,r_0[k],size[l],vel[m])
 
 np.save('outflows1', results)
