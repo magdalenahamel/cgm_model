ee05f490257be402d5c49269b23238b8e6f2d493
magdalenahamel <30760661+magdalenahamel@users.noreply.github.com>
2021-12-09 18:24:17 +0100

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -76,7 +76,7 @@ def sin_i_dist(y, ymin):
 
 sinivals = np.linspace(np.sin(np.radians(5.7)),0.99,100)
 f_D_i = RanDist(sinivals, sin_i_dist(sinivals,np.radians(5.7)))
-print('exi', sin_i_dist(sinivals,np.radians(5.7)))
+print('exi', f_D_i.random(10))
 
 '''Doppler parameter distribution'''
 
