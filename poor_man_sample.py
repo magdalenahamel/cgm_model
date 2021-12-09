0ffad3cd7a3c5e77c3a4e786e639c1479993e78c
magdalenahamel <30760661+magdalenahamel@users.noreply.github.com>
2021-12-09 12:55:32 +0100

    poor_man_py to multiprocess

--- poor_man_sample.py
+++ poor_man_sample.py
@@ -7,106 +7,30 @@ import matplotlib.pyplot as plt
 
 import cgmspec.Sample as sample
 
-###possible filling factor functions
+import concurrent.futures
 
-def log_log(r,a,b):
-    return(10**(a) * r**(b))
+###possible filling factor functions
 
 def prob_hit_log_lin(r, r_vir, a, b, por_r_vir = 0.5):
     r_t = r/r_vir
     return(np.exp(a)*(np.exp(-b*r_t)))
 
-
-def prob_hit_don(r, r_vir, prob_rc, rmax, por_r_vir = 0.5):
-        """
-        Probability of hitting a cloud at distance r in the plane xy of a disc of radius rmax. For the moment is a power law.
-
-        :param r: np.array, array with distances to the center of disc in plane xy in kpc
-        :param r_0: float, the characteristicradius of the power law
-        :param prob_rmin: float, probability at Rcore or below, default 100% probability of crossing a cloud
-        :return: float, probability of hitting a cloud
-        """
-
-        prob = np.ones(len(r)) * prob_rc
-        prob = np.where(r<=por_r_vir*r_vir,0, prob)
-        prob = np.where(r>rmax*r_vir, 0, prob)
-        return prob
-
-def prob_hit_don_don(r, r_vir, prob_rc, rmax, por_r_vir = 0.5):
-        """
-        Probability of hitting a cloud at distance r in the plane xy of a disc of radius rmax. For the moment is a power law.
-
-        :param r: np.array, array with distances to the center of disc in plane xy in kpc
-        :param r_0: float, the characteristicradius of the power law
-        :param prob_rmin: float, probability at Rcore or below, default 100% probability of crossing a cloud
-        :return: float, probability of hitting a cloud
-        """
-
-        prob = np.ones(len(r)) * prob_rc
-        prob = np.where(r>por_r_vir*r_vir,0, prob)
-        prob = np.where(r>rmax*r_vir, 0, prob)
-        return prob
-
-
-
-def prob_hit_exp(r, r_vir, prob_rc, rmax, por_r_vir = 0.5):
-        """
-        Probability of hitting a cloud at distance r in the plane xy of a disc of radius rmax. For the moment is a power law.
-
-        :param r: np.array, array with distances to the center of disc in plane xy in kpc
-        :param r_0: float, the characteristicradius of the power law
-        :param prob_rmin: float, probability at Rcore or below, default 100% probability of crossing a cloud
-        :return: float, probability of hitting a cloud
-        """
-        print(r_vir, prob_rc,rmax,por_r_vir)
-        rmin = 0.1
-        A = 100
-        B = -(por_r_vir) * (1/np.log(prob_rc/100))
-        prob = A*np.exp(-r/(B*r_vir))
-        prob = np.where(r<=rmin, 100, prob)
-        prob = np.where(r>rmax*r_vir, 0, prob)
-        return prob
-
-def prob_hit_pow_law(r, r_vir, prob_rc, rmax, por_r_vir = 0.5):
-        """
-        Probability of hitting a cloud at distance r in the plane xy of a disc of radius rmax. For the moment is a power law.
-
-        :param r: np.array, array with distances to the center of disc in plane xy in kpc
-        :param r_0: float, the characteristicradius of the power law
-        :param prob_rmin: float, probability at Rcore or below, default 100% probability of crossing a cloud
-        :return: float, probability of hitting a cloud
-        """
-        rmin = 0.1
-        ind = np.log(prob_rc/100)/(np.log(por_r_vir*r_vir)-np.log(rmin))
-        b = r/r_vir
-        A = prob_rc/((por_r_vir)**ind)
-        prob = A * (b**(ind))
-        prob = np.where(r<=rmin,100, prob)
-        prob = np.where(prob>100,100, prob)
-        prob = np.where(r>rmax*r_vir, 0, prob)
-        return prob
-
-
 #### define grids for the poor mans mcmc
-bs = np.logspace(0.1,1.5,10) 
-csize = np.logspace(0.01,1,10)
-hs = 5
-hv = 10
 
-#bs = np.linspace(0.1,4,7) # characteristic radius of the exponential function (it is accually a porcentage of Rvir) in log scale to make the range more homogeneous in lin scale
-#csize = np.linspace(0.01,1,7) #poner en escala mas separada
-#hs = np.linspace(1,20,7) #bajar un poco para que no sea un  1,10,20
-#hv = np.linspace(0, 20,7) #bajar maximo a 100
+bs = np.linspace(0.1,4,7) # characteristic radius of the exponential function (it is accually a porcentage of Rvir) in log scale to make the range more homogeneous in lin scale
+csize = np.linspace(0.01,1,7) #poner en escala mas separada
+hs = np.linspace(1,20,7) #bajar un poco para que no sea un  1,10,20
+hv = np.linspace(0, 20,7) #bajar maximo a 100
 
 params = [bs,csize,hs,hv]
 
 
 
 ### run the model in the parameter grid
-results_Wr = []
+'''results_Wr = []
 results_D = []
 results_R_vir = []
-results_specs = []
+results_specs = []'''
 
 '''for l in range(len(bs)):
     for i in range(len(csize)):
@@ -119,7 +43,7 @@ results_specs = []
                 results_D.append(e3_a_1[3])
                 results_R_vir.append(e3_a_1[7])'''
 
-for l in range(len(bs)):
+'''for l in range(len(bs)):
     for i in range(len(csize)):
         print(l,i)
         exp_fill_fac = sample.Sample(prob_hit_log_lin,200,sample_size=300, csize=csize[i], h=hs, hv=hv)
@@ -133,7 +57,37 @@ for l in range(len(bs)):
 results_Wr_r = np.reshape(results_Wr, (10,10,300))
 results_D_r = np.reshape(results_D, (10,10,300))
 results_R_vir_r = np.reshape(results_R_vir, (10,10,300))
+results_r = [results_Wr_r, results_D_r, results_R_vir_r]'''
+
+### Multiprocess ###
+
+groups = list(itertools.product(bs,csize,hs,hv))
+
+def get_sample(bs, csize, hs, h_v):
+    exp_fill_fac = sample.Sample(prob_hit_log_lin,200,sample_size=300, csize=csize, h=hs, hv=hv)
+    e3_a_1 = exp_fill_fac.Nielsen_sample(np.log(100),bs,0.2)
+    Wr = e3_a_1[8]
+    D = e3_a_1[3]
+    R_vir = e3_a_1[7]
+    specs = e3_a_1[1]
+    return(Wr, D, R_vir, specs)
+    
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    results = executor.map(get_sample, groups)
+    
+
+    
+EW_results = results[:][0]
+D_results = results[:][1]
+R_vir_results = results[:][2]
+specs_results = results[:][3]
+
+EW_r = np.reshape(EW_results, (7,7,7,7,300))
+D_r = np.reshape(D_results, (7,7,7,7,300))
+R_vir_r = np.reshape(R_vir_results, (7,7,7,7,300))
+specs_r = np.reshape(specs_results, (7,7,7,7,300,len(wave))
 results_r = [results_Wr_r, results_D_r, results_R_vir_r]
 
-np.save('mcmc_5', results_r)
-np.save('mcmc_5_specs',results_specs)
+
+np.save('mp_mcmc_6', results_r)
+np.save('mp_mcmc_6_specs',specs_r)
